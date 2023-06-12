80c5f53fd58d233483ba14b8ef7a255bc9934c67
Ruslan Ukhlin <ruslanukhlin@gmail.com>
2023-06-12 20:26:14 +0300

    добавил в конфиги параметры прода

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,7 @@
 export const BackPort = 'http://localhost:5000/';
 export const WsBackPort = 'ws://localhost:5000/';
 export const SocketIoPort = 'http://localhost:4000/';
+
+// export const BackPort = 'https://server-storylifesocial.ru/';
+// export const WsBackPort = 'wss://server-storylifesocial.ru/';
+// export const SocketIoPort = 'http://socket-storylifesocial.ru/';
